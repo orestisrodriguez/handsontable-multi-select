c89337933080afe2b817fe73a238a4826a18d891
Ludwig Magnusson <ludwig@mediatool.com>
2020-02-24 13:21:07 +0100

    feat: add ability for numeric options and max selections
    The plugin now takes the type option into account. If the type is
    `numeric` the plugin will return a `Number` value just like regular
    handsontable cells.
    
    Is is also possible to set `maxItemCount`. This will limit the number of
    possible choices. Currently it only makes sense to use the limit 1 with
    a numeric cell since joined values always will be a string.

--- web/main.js
+++ web/main.js
@@ -1,11 +1,13 @@
-import { pluck } from 'ramda'
+import { pluck, times } from 'ramda'
 import { MultiSelectEditor, MultiSelectRenderer } from '../lib/multi-select'
 import data from './users'
 import options from './options'
 
 const sheet = document.getElementById('sheet')
 
-const headers = [ 'First name', 'Last name', 'Email', 'Job title', 'Country' ]
+const headers = [ 'First name', 'Last name', 'Email', 'Job title', 'Country', 'Single Number', 'Multi Numbers' ]
+
+const numberOptions = times((i) => ({ key: i, value: i }), 50)
 
 new Handsontable(sheet, {
   data,
@@ -20,6 +22,7 @@ new Handsontable(sheet, {
     {},
     {},
     {
+      type: 'text',
       editor: MultiSelectEditor,
       renderer: MultiSelectRenderer,
       select: {
@@ -31,6 +34,23 @@ new Handsontable(sheet, {
         options (source, process) {
           return new Promise((resolve) => {
             setTimeout(resolve, 500, options)
+          })
+        },
+      }
+    }, {
+      type: 'numeric',
+      editor: MultiSelectEditor,
+      renderer: MultiSelectRenderer,
+      select: {
+        config: {
+          valueKey: 'key',
+          labelKey: 'text',
+          separator: ';',
+          maxItemCount: 1,
+        },
+        options (source, process) {
+          return new Promise((resolve) => {
+            setTimeout(resolve, 500, numberOptions)
           })
         },
       }
